ee8cb6a1b4fbf4ae76f75c10acd932db738d8dd0
Ethan Harlig <ethan.harlig@gmail.com>
2017-12-16 01:55:51 -0800

    Made stuff look cleaner by adding <hr/> and new font

--- src/home/home.js
+++ src/home/home.js
@@ -22,19 +22,25 @@ const Home = props => {
             <ListGroup>
               <ListGroupItem>
                 <ListGroupItemHeading>
-                  So who are you and just who do you think you are
-                  putting a huge picture of you and your dog up there?
+                  So who are you and just who do you think you are putting a
+                  huge picture of you and your dog up there?
                 </ListGroupItemHeading>
+                <hr />
                 <ListGroupItemText>
                   Howdy! My name is Ethan Harlig and I am a fourth-year student
                   studying computer science at California Polytechnic State
                   University, San Luis Obispo.
                 </ListGroupItemText>
                 <ListGroupItemText>
-                  I am also an Associate Software Developer at {' '}
-                  <a href="http://www.thinglogix.com" target="_blank" rel="noopener noreferrer">ThingLogix</a>, an IoT
-                  company. I architect and develop Internet of Things solutions
-                  built on AWS.
+                  I am also an Associate Software Developer at{' '}
+                  <a
+                    href="http://www.thinglogix.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    ThingLogix
+                  </a>, an IoT company. I architect and develop Internet of
+                  Things solutions built on AWS.
                 </ListGroupItemText>
                 <ListGroupItemText>
                   I have a wonderful 12-year old dog named Tommy (pictured
@@ -47,6 +53,7 @@ const Home = props => {
                 <ListGroupItemHeading>
                   So what technologies do you know?
                 </ListGroupItemHeading>
+              <hr/>
                 <ListGroupItemText>
                   I'm glad you asked! I am proficient with:
                   <ul>
@@ -62,12 +69,14 @@ const Home = props => {
                     <li>MySQL</li>
                     <li>NoSQL</li>
                   </ul>
+                  <br />
                   And I dabble with:
                   <ul>
                     <li>Node.js</li>
                     <li>ReactJS (this site)</li>
                     <li>NativeScript</li>
                   </ul>
+                  <br />
                   I also work a lot with AWS, specifically:
                   <ul>
                     <li>Lambda</li>
@@ -84,22 +93,43 @@ const Home = props => {
                 <ListGroupItemHeading>
                   Tell me about your personal projects.
                 </ListGroupItemHeading>
+              <hr/>
                 <ListGroupItemText>
-                  I have recently been heavily invested in a personal project, {' '}
-                  <a href="https://ridetrackr.io" target="_blank" rel="noopener noreferrer">RideTrackr</a>, where I lead
-                  backend development and worked on the frontend in conjunction
-                  with {' '}
-                  <a href="https://github.com/collindutter" target="_blank" rel="noopener noreferrer">Collin Dutter</a>.
+                  I have recently been heavily invested in a personal project,{' '}
+                  <a
+                    href="https://ridetrackr.io"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    RideTrackr
+                  </a>, where I lead backend development and worked on the
+                  frontend in conjunction with{' '}
+                  <a
+                    href="https://github.com/collindutter"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Collin Dutter
+                  </a>.
                 </ListGroupItemText>
                 <ListGroupItemText>
                   I also write a bunch of code, but a lot of it tends to be
                   small scripts, like Slack bots, that I don't end up uploading
                   to my{' '}
-                  <a href="https://github.com/ethanharlig" target="_blank" rel="noopener noreferrer">GitHub</a>.
+                  <a
+                    href="https://github.com/ethanharlig"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    GitHub
+                  </a>.
                 </ListGroupItemText>
                 <ListGroupItemText>
                   If you are interested in checking out more of my projects,
-                  mosey on over to my <a href="/projects" target="_blank">projects page</a>!
+                  mosey on over to my{' '}
+                  <a href="/projects" target="_blank">
+                    projects page
+                  </a>!
                 </ListGroupItemText>
               </ListGroupItem>
               <ListGroupItem>
@@ -107,11 +137,18 @@ const Home = props => {
                   Holy moly you sound like someone I want to talk to. Just how
                   do I reach you?
                 </ListGroupItemHeading>
+              <hr/>
                 <ListGroupItemText>
                   If you like what you see, or you are interested in
-                  collaborating on anything, feel free to {' '}
-                  <a href="mailto:ethan.harlig@gmail.com" target="_blank" rel="noopener noreferrer">email me</a> and we
-                  can talk!
+                  collaborating on anything, feel free to{' '}
+                  <a
+                    href="mailto:ethan.harlig@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    email me
+                  </a>{' '}
+                  and we can talk!
                 </ListGroupItemText>
               </ListGroupItem>
             </ListGroup>
